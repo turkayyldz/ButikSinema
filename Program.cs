88bc429a7b4e0b7c04bb4e383a7e3d96efb0fddb
turkayyldz <79602292+turkayyldz@users.noreply.github.com>
2022-10-28 11:39:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace ButikSinema
+{
+    class Program
+    {
+        static Sinema Snm;
+        static void Main(string[] args)
+        {
+            Uygulama();
+           // Snm.CiroHesapla();
+        }
+        static void Uygulama()
+        {
+            Giris();
+            Menu();
+            while (true)
+            {
+                Console.WriteLine();
+                string giris = SecimAl();
+                Console.WriteLine();
+                switch (giris)
+                {
+                    case "1":
+                    case "S":
+                        BiletSat();
+                        break;
+                    case "2":
+                    case "R":
+                        BiletIade();
+                        break;
+                    case "3":
+                    case "D":
+                        DurumBilgisi();
+                        break;
+                    case "4":
+                    case "X":
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+        static void Giris()
+        {
+            Console.WriteLine("-------Butik Sinema Salonu-------");
+            Console.Write("Film adı: ");
+            string film = Console.ReadLine();
+            Console.Write("Kapasite:");
+            int kap = int.Parse(Console.ReadLine());
+            Console.Write("Tam Bilet Fiyatı: ");
+            float tam = float.Parse(Console.ReadLine());
+            Console.Write("Yarım Bilet Fiyatı:");
+            float yarim = float.Parse(Console.ReadLine());
+
+            Snm = new Sinema(film, kap, tam, yarim);
+        }
+        static void Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1 - Bilet Sat(S) ");
+            Console.WriteLine("2 - Bilet İade(R)");
+            Console.WriteLine("3 - Durum Bilgisi(D)");
+            Console.WriteLine("4 - Çıkış(X)");
+        }
+        static string SecimAl()
+        {
+            int sayac = 0;
+            string karakterler = "1234SRDX";
+
+            while (true)
+            {
+                sayac++;
+                Console.Write("Seçiminiz:");
+                string giris = Console.ReadLine().ToUpper();
+
+                if (giris.Length == 1 && karakterler.IndexOf(giris) > -1)
+                {
+                    return giris;
+                }
+                else if (sayac == 10)
+                {
+                    Console.WriteLine("Üzgünüm sizi anlayamıyorum. Program sonlandırılıyor.");
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine("Hatalı giriş yapıldı. Tekrar deneyin.");
+            }
+        } 
+        static void BiletSat() 
+        {
+            Console.WriteLine("Bilet Sat:");
+
+            // Console.Write("Tam Bilet Adedi: "); int.Parse(Console.ReadLine());
+            int tam = SayiAl("Tam Bilet Adedi: ");
+
+            // Console.Write("Yarım Bilet Adedi: "); int.Parse(Console.ReadLine());
+            int yarim = SayiAl("Yarım Bilet Adedi: ");
+            //Snm.ToplamTamBiletAdedi += tamBiletadedi;
+            //Snm.ToplamYarimBiletAdedi += yarimBiletadedi;
+            Snm.BiletSatis(tam, yarim);
+        }
+        static void BiletIade()
+        {
+            Console.WriteLine("Bilet İade:");            
+            int tam = SayiAl("Tam Bilet Adedi: ");        
+            int yarim = SayiAl("Yarım Bilet Adedi: ");
+            Snm.BiletIadesi(tam, yarim);
+        }
+        static void DurumBilgisi()
+        {
+            Console.WriteLine("Durum Bilgisi");
+            Console.WriteLine("Film: " + Snm.FilmAdi);
+            Console.WriteLine("Kapasite: " + Snm.Kapasite);
+            Console.WriteLine("Tam Bilet Fiyatı: " + Snm.TamBiletFiyati);
+            Console.WriteLine("Yarım Bilet Fiyatı: " + Snm.YarimBiletFiyati);
+            Console.WriteLine("Toplam Tam Bilet Adedi: " + Snm.ToplamTamBiletAdedi);
+            Console.WriteLine("Toplam Yarım Bilet Adedi: " + Snm.ToplamYarimBiletAdedi);
+            Console.WriteLine("Ciro: " + Snm.Ciro);
+            Console.WriteLine("Boş Koltuk Adedi: " + Snm.BosKoltukAdediGetir());
+        }
+        static int SayiAl(string mesaj)
+        {
+            int sayi;
+            while (true)
+            {
+                Console.Write(mesaj);
+                string giris = Console.ReadLine();
+
+                if (int.TryParse(giris, out sayi))
+                {
+                    return sayi;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı giriş yapıldı.");
+                }
+            }
+        }
+    }
+}
---
+++ Sinema.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikSinema
+{
+    class Sinema
+    {
+        public string FilmAdi { get; set; }
+        public int Kapasite { get; }
+        public float TamBiletFiyati { get; }
+        public float YarimBiletFiyati { get; }
+        public int ToplamTamBiletAdedi { get; private set; }
+        public int ToplamYarimBiletAdedi { get; private set; }
+        public float Ciro
+        {
+            get
+            {
+                return this.ToplamTamBiletAdedi * this.TamBiletFiyati + this.ToplamYarimBiletAdedi * this.YarimBiletFiyati;
+            }
+        }
+        public Sinema(string filmAdi, int kapasite, float tamBiletFiyati, float yarimBiletFiyati)
+        {
+            this.FilmAdi = filmAdi;
+            this.Kapasite = kapasite;
+            this.YarimBiletFiyati = yarimBiletFiyati;
+            this.TamBiletFiyati = tamBiletFiyati;
+        }
+        public void BiletSatis(int tamBiletAdedi, int yarimBiletAdedi)
+        {
+            if (BosKoltukAdediGetir() >= tamBiletAdedi + yarimBiletAdedi)
+            {
+                this.ToplamTamBiletAdedi += tamBiletAdedi;
+                this.ToplamYarimBiletAdedi += yarimBiletAdedi;
+                CiroHesapla();
+                Console.WriteLine("İşlem gerçekleştirildi.");
+            }
+            else
+            {
+                Console.WriteLine("Yeterli boş koltuk yok. Satılabilecek bilet adedi " + BosKoltukAdediGetir());
+            }
+        }
+        public void BiletIadesi(int tamBiletAdedi, int yarimBiletAdedi) 
+        {
+            if (tamBiletAdedi <= this.ToplamTamBiletAdedi && yarimBiletAdedi <= this.ToplamYarimBiletAdedi)
+            {
+                this.ToplamTamBiletAdedi -= tamBiletAdedi;
+                this.ToplamYarimBiletAdedi -= yarimBiletAdedi;
+                CiroHesapla();
+                Console.WriteLine("İşlem gerçekleştirildi.");
+            }
+            else
+            {
+                Console.WriteLine("İade edilebilecek bilet miktarını aştınız.");
+            }
+        }
+        private void CiroHesapla()
+        {
+            // this.Ciro = this.ToplamTamBiletAdedi * this.TamBiletFiyati + this.ToplamYarimBiletAdedi * this.YarimBiletFiyati;
+        }
+        public int BosKoltukAdediGetir()
+        {
+            int adet = this.Kapasite - this.ToplamTamBiletAdedi - this.ToplamYarimBiletAdedi;
+            return adet;
+        }
+    }
+}
